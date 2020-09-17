0a024624e0234ac0440a9ac4b7a2abcbbb2f3694
HernandezM-dev <HernandezM.dev@gmail.com>
2020-09-16 23:20:37 -0400

    created enhancer functions

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -6,17 +6,36 @@ module.exports = {
 };
 
 function success(item) {
-  return { ...item };
+  return item.enhancement === 20 
+  ? 
+  {...item} 
+  :
+   { ...item, enhancement: item.enhancement + 1 };
 }
 
 function fail(item) {
-  return { ...item };
+  if(item.enhancement < 15){
+    return {...item, durability: item.durability - 5}
+  }else{
+      if(item.enhancement > 16){
+       return{...item, enhancement: item.enhancement -1, durability: item.durability - 10}
+      }else{
+        return {...item, durability: item.durability - 10}
+      }
+  }
 }
 
 function repair(item) {
-  return { ...item };
+  return { ...item, durability: 100 };
 }
 
 function get(item) {
-  return { ...item };
+    return { ...item, get: function(){
+      if this.enhancement === 0 {
+        return {...item}
+      }else{
+        return {...item, name: `[+${this.enhancement}]${this.name}`}
+      }
+    } 
+  }
 }
